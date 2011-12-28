df64eb0a9a8d5a0034c56ecea4b041d99519d23e
Ashleigh Baumgardner <labaumgardner@gmail.com>
2011-12-27 21:28:19 -0800

    Precompiled assets

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,8 +1,2 @@
 MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
 MongoMapper.database = "touring-#{Rails.env}"
-
-if defined?(PhusionPassenger)
-   PhusionPassenger.on_event(:starting_worker_process) do |forked|
-     MongoMapper.connection.connect if forked
-   end
-end
